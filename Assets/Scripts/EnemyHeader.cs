9b9beeae16b4c143cdc254d35057dc17c1eb26de
Bilal Aydin <bilal@arvisgames.com>
2023-04-22 18:37:38 +0300

    Added header for enemy and created enemy prefab.

---
+++ Assets/Scripts/EnemyHeader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class EnemyHeader: MonoBehaviour
+{
+    public string HeaderMessage;
+    public TextMeshProUGUI HeaderText;
+
+    private void Awake()
+    {
+        HeaderText.text = HeaderMessage;
+    }
+}
